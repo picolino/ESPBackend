76ceeae93c2ad207cff1ce997809f2951ecd7378
Данила Червонный <webmaster.pico@gmail.com>
2017-12-21 02:14:47 +0300

    Рефакторинг

---
+++ Authorization/Domain/UserModel.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Authorization.Domain
+{
+    public class UserModel : IdentityUser
+    {
+        
+    }
+}
--- Authorization/Models/AuthDbContext.cs
+++ Authorization/Models/AuthDbContext.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
+﻿using Authorization.Domain;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace Authorization.Models
 {
-    public class AuthDbContext : IdentityDbContext<IdentityUser>
+    public class AuthDbContext : IdentityDbContext<UserModel>
     {
         public AuthDbContext()
             : base("AuthDbContext")
         {
         }
+
+        public static AuthDbContext Create()
+        {
+            return new AuthDbContext();
+        }
+
+
     }
 }
