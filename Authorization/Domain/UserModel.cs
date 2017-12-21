31274e421de1a25dc734ea1dc732d1cb1f3c9fcc
Данила Червонный <webmaster.pico@gmail.com>
2017-12-21 03:24:44 +0300

    Добавление таблиц для идентификации

--- Authorization/Domain/UserModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
-
-namespace Authorization.Domain
-{
-    public class UserModel : IdentityUser
-    {
-        
-    }
-}
--- Authorization/Models/AuthDbContext.cs
+++ Authorization/Models/AuthDbContext.cs
@@ -1,20 +1,25 @@
-﻿using Authorization.Domain;
-using Microsoft.AspNet.Identity.EntityFramework;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace Authorization.Models
 {
-    public class AuthDbContext : IdentityDbContext<UserModel>
+    public class AuthDbContext : IdentityDbContext
     {
         public AuthDbContext()
             : base("AuthDbContext")
         {
         }
 
-        public static AuthDbContext Create()
+        protected override void OnModelCreating(System.Data.Entity.DbModelBuilder modelBuilder)
         {
-            return new AuthDbContext();
+            base.OnModelCreating(modelBuilder);
+
+
+            modelBuilder.Entity<IdentityUser>().ToTable("Users", "dbo");
+            modelBuilder.Entity<IdentityRole>().ToTable("Roles", "dbo");
+            modelBuilder.Entity<IdentityUserRole>().ToTable("UserRoles", "dbo");
+            modelBuilder.Entity<IdentityUserClaim>().ToTable("UserClaims", "dbo");
+            modelBuilder.Entity<IdentityUserLogin>().ToTable("UserLogins", "dbo");
+
         }
-
-
     }
 }
