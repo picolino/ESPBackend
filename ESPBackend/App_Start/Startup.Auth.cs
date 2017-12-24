f547babeedfbf5573cd3296ca1ebc8f162778659
Данила Червонный <webmaster.pico@gmail.com>
2017-12-24 23:20:59 +0300

    Добавление аутентификации в другой сервер

--- ESPBackend/App_Start/Startup.Auth.cs
+++ ESPBackend/App_Start/Startup.Auth.cs
@@ -1,15 +1,18 @@
 ﻿#region Usings
 
+using ESPBackend;
+using Microsoft.Owin;
 using Microsoft.Owin.Security.OAuth;
 using Owin;
 
 #endregion
 
+[assembly: OwinStartup(typeof(Startup))]
 namespace ESPBackend
 {
     public class Startup
     {
-        public void ConfigureAuth(IAppBuilder app)
+        public void Configuration(IAppBuilder app)
         {
             app.UseOAuthBearerTokens(new OAuthAuthorizationServerOptions());
         }
