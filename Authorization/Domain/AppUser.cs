f523ee04f40e527b7a67849c50a59de12d7c12bc
Данила Червонный <webmaster.pico@gmail.com>
2018-01-28 16:10:37 +0300

    Добавление аутентификации через гугл. Регистрация провайдера и добавление контекста пользователя

---
+++ Authorization/Domain/AppUser.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Authorization.Domain
+{
+    public class AppUser : IdentityUser
+    {
+        public bool IsGoogleAuthenticatorEnabled { get; set; }
+
+        public string GoogleAuthenticatorSecretKey { get; set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<AppUser> manager)
+        {
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            return userIdentity;
+        }
+    }
+}
---
+++ Authorization/Providers/GoogleAuthenticatorTokenProvider.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using Authorization.Domain;
+using Base32;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using OtpSharp;
+
+namespace Authorization.Providers
+{
+    public class GoogleAuthenticatorTokenProvider : IUserTokenProvider<AppUser, string>
+    {
+        public Task<string> GenerateAsync(string purpose, UserManager<AppUser, string> manager, AppUser user)
+        {
+            return Task.FromResult((string)null);
+        }
+
+        public Task<bool> ValidateAsync(string purpose, string token, UserManager<AppUser, string> manager, AppUser user)
+        {
+            long timeStepMatched = 0;
+
+            var otp = new Totp(Base32Encoder.Decode(user.GoogleAuthenticatorSecretKey));
+            var valid = otp.VerifyTotp(token, out timeStepMatched, new VerificationWindow(2, 2));
+
+            return Task.FromResult(valid);
+        }
+
+        public Task NotifyAsync(string token, UserManager<AppUser, string> manager, AppUser user)
+        {
+            return Task.FromResult(true);
+        }
+
+        public Task<bool> IsValidProviderForUserAsync(UserManager<AppUser, string> manager, AppUser user)
+        {
+            return Task.FromResult(user.IsGoogleAuthenticatorEnabled);
+        }
+    }
+}
